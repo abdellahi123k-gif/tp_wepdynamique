cfdca4b69c26274052a84d8625b6f9973ecf9c75
abdellahi <abdellahi123k@gmail.com>
2026-05-05 17:34:39 +0000

    test de travaille

--- src/main/java/com/example/demo/Repository/UserRepository.java
+++ src/main/java/com/example/demo/Repository/UserRepository.java
@@ -2,10 +2,10 @@ package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.Entity.LivreEntity;
+import com.example.demo.Entity.UserEntity;
 
 @Repository  // يساعد Spring على اكتشاف هذا الـ Repository تلقائياً
-interface LivreRepository extends JpaRepository<LivreEntity, Long> {
+public interface UserRepository  extends JpaRepository<UserEntity, Long> {
 
     // ✅ لا تكتب أي كود هنا!
     // ✅ JpaRepository يمنحك تلقائياً أكثر من 15 دالة جاهزة
---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import com.example.demo.Repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.Entity.UserEntity;
+import java.util.List;
+
+
+@Controller
+public class HomeController {
+
+    @Autowired
+     private UserRepository userRepository;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<UserEntity> users = userRepository.findAll();
+        model.addAttribute("users", users);
+        return "index";
+    }
+    @PostConstruct
+    public void init() {
+        userRepository.save(new UserEntity(null, "Ali"));
+        userRepository.save(new UserEntity(null, "Sara"));
+    }
+    @PostMapping("/add")
+    public String addUser(@RequestParam String name) {
+
+        userRepository.save(new UserEntity(null, name));
+
+        return "redirect:/";
+    }
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+
+        userRepository.deleteById(id);
+
+        return "redirect:/";
+    }
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable Long id, Model model) {
+
+        UserEntity user = userRepository.findById(id).orElse(null);
+
+        model.addAttribute("user", user);
+
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(UserEntity user) {
+
+        userRepository.save(user);
+
+        return "redirect:/";
+    }
+
+
+
+}
+
